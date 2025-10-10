c896d057d9c66d49e1acf7a745bdb804663f8e39
Raven-504 <134726537+Raven-504@users.noreply.github.com>
2025-10-10 13:54:48 +0000

    Update thing3.js

--- thing3/thing3.js
+++ thing3/thing3.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const camCoords = [0, -100];
 const points = [];
 const connections = [];
 
@@ -23,24 +24,36 @@ ctx.moveTo(centreX, 0);
 ctx.lineTo(centreX, canvas.height);
 ctx.stroke();
 
+ctx.strokeStyle = "black";
+ctx.fillStyle = "black";
 
-points[0] = [1,0]
-points[1] = [2,3]
-console.log(points)
-
+points[0] = [100,100]
+points[1] = [-100,100]
 
 function draw() {
-	//line from camera to projection
-
-	//line form camera to object
-
-	//draw camera
-
+	const camX = camCoords[0]
+	const camY = camCoords[1]
+	
+	//line from camera to object to projection
+	for (let i = 0; i < points.length; i++) {
+		const objX = points[i][0];
+		const objY = points[i][1];
+		
+		const projX = centreX + objX * -camY / (-camY + objY);
+		
+		ctx.beginPath();
+		ctx.moveTo(centreX + camX, centreY - camY);
+		ctx.lineTo(centreX + objX, centreY - objY);
+		ctx.lineTo(projX, centreY)
+		ctx.stroke();
+	}
 	//draw objects
 
 	//draw projected points
-
+	
+	//draw camera
+	
 	//projected line
 }
-
+draw()
 //drag nonsense
